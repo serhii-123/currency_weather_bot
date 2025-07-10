834c415cef012cf80798f60bb282e26d121311cd
serhii-123 <46873711+serhii-123@users.noreply.github.com>
2025-07-10 11:55:58 +0300

    Create index.ts

---
+++ classes/index.ts
@@ -0,0 +1,17 @@
+import CurrencyDataFetcher from "./CurrencyDataFetcher";
+import CurrencyMsgBuilder from "./CurrencyMsgBuilder";
+import CurrencyError from "./CurrencyError";
+import WeatherDataFetcher from "./WeatherDataFetcher";
+import WeatherMsgBuilder from './WeatherMsgBuilder';
+import WeatherError from "./WeatherError";
+import WeatherConversations from "./WeatherConversations";
+
+export {
+    CurrencyDataFetcher,
+    CurrencyMsgBuilder,
+    CurrencyError,
+    WeatherDataFetcher,
+    WeatherMsgBuilder,
+    WeatherError,
+    WeatherConversations
+};
